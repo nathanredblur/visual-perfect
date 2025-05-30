23da5f34143752c638ae62ebe080b08f4eb4515e
Jonathan Esteban Rico <jon.nathan.rich@gmail.com>
2025-05-30 16:48:34 -0500

    Update addon identifiers and remove unused Tab component

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,7 @@
-export const ADDON_ID = "my-addon";
+export const ADDON_ID = "visual-perfect";
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const PANEL_ID = `${ADDON_ID}/panel`;
-export const TAB_ID = `${ADDON_ID}/tab`;
-export const KEY = `my-addon`;
+export const KEY = `visual-perfect`;
 
 export const EVENTS = {
   RESULT: `${ADDON_ID}/result`,
